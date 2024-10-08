45684de291abbbbc196e0d3011c6eb98410aa9ce
maib07001 <maib07001@E-PW05C37V>
2024-10-08 14:24:44 +0200

    Add project files.

---
+++ uppgift .1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace uppgift_1._1
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur många dagar vill du hyra bilen");
+            int x = int.Parse(Console.ReadLine());
+            int antaldagar = (x * 500) - 500;
+            int dagsum = 300 + antaldagar;
+
+            Console.WriteLine("hur många km ska du köra?");
+            int km = int.Parse(Console.ReadLine());
+            int kilometer = km * 1;
+            int sum = kilometer + dagsum;
+
+            Console.WriteLine("summan är" + sum + "kronor");
+
+
+        }
+
+
+    }
+
+
+
+}
